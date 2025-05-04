2163ddc8df883158574275b2a378b1bcb8fc7cfc
Arek2208 <Ower123@interia.pl>
2025-05-04 20:58:24 +0200

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+imie_odbiorcy = input("Podaj imię odbiorcy: ")
+imie_odbiorcy = print("Ania")
+imie_odbiorcy = ('Ania')
+rok_teraz = 2025
+rok_urodzenia = input("Podaj rok urodzenia:")
+print(f"Twoj wiek to: {rok_teraz -int(rok_urodzenia)}")
+wiek = rok_teraz -int(rok_urodzenia)
+wiadomosc = input("Podaj spersonalizowaną wiadomość: ")
+wiadomosc = print("co u ciebie słychać , mam nadzieje ,ze wkrotce sie zobaczymy")
+imie_nadawcy = input("Podaj imie nadawcy: ")
+imie_nadawcy = print("Arek")
+imie_nadawcy = ('Arek')
+
+print(f"{imie_odbiorcy}, wszystkiego najlepszego z okazji {wiek} urodzin!\n")
+
+print(f"Pozdrawiam,\n{imie_nadawcy}")
+
+
